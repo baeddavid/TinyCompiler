6fe9d6aa24bd172efc5cdddafaefe31d7ec4c078
David <baeddavid@gmail.com>
2021-11-13 20:28:07 -0600

    add method to check if input is a keyword

--- Token.java
+++ Token.java
@@ -6,4 +6,17 @@ public class Token {
         text = tokenText;
         kind = tokenKind;
     }
+
+    public static Enum checkIfKeyword(String tokenText) {
+        try {
+            TokenType candidate = TokenType.valueOf(tokenText);
+            if(candidate.getKey().equals(tokenText) && candidate.getValue() >= 100 && candidate.getValue() < 200) {
+                return candidate;
+            }
+        }
+        catch(IllegalArgumentException i) {
+            return null;
+        }
+        return null;
+    }
 }
